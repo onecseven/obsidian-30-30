e2323bbcf4b110670e13873652d61ad6bd18d841
https://github.com/onecseven <onecseven@gmail.com>
2022-12-30 19:23:22 -0300

    making some unit tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,7 +1,7 @@
 import { MarkdownView } from "obsidian"
 import { Parser } from "../src/parser"
+import { Task } from "../src/primitives"
 import { describe, expect, test } from "@jest/globals"
-
 let test_text = `#+ Research
 25 Readings
 ss
@@ -44,18 +44,30 @@ Algorithm: 25
 
 lol
 `
+let title_with_label = new RegExp(/\#\+\s*.+/g)
+Parser.Commands.test("#+ apolo")
+let title = new RegExp(/\#\+\s*/g)
+title.test("#+")
+Parser.Commands.test("#+")
 
-describe("testing to see if parser works", () => {
+describe("Tasklist Block Parser", () => {
   test("string should be parsed as a block", () => {
-    expect(
-      Parser.contains_tasklist_block({ data: test_text } as MarkdownView)
-    ).toBe(true)
+    expect(Parser.contains_tasklist({ data: test_text } as MarkdownView)).toBe(
+      true
+    )
   }),
-  test("string should not be parsed as a block", () => {
-    expect(
-      Parser.contains_tasklist_block({ data: bad_text } as MarkdownView)
-    ).toBe(false)
-  })
+    test("string should not be parsed as a block", () => {
+      expect(Parser.contains_tasklist({ data: bad_text } as MarkdownView)).toBe(
+        false
+      )
+    })
+})
+
+describe("Task Parser", () => {
+test(" Describe Test", ()=> {
+expect().toBe()
+})
+
 })
 
 export {}
