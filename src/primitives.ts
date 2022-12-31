4ddf961e14b042d0119c00ec8abbf4a80252b99a
https://github.com/onecseven <onecseven@gmail.com>
2022-12-31 19:26:40 -0300

    trying to figure this out

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,8 +1,10 @@
 function getUniqueID() {
   function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
   }
-  return s4() + s4() + '-' + s4();
+  return s4() + s4() + "-" + s4()
 }
 
 export class StopTask implements StopTask {
@@ -10,7 +12,15 @@ export class StopTask implements StopTask {
   name = "BREAK"
   length = 0
   remaining_length = 0
-  tick() {
+  stopcall: () => void
+  constructor(stopcall: () => void) {
+    console.log("stopped")
+    this.stopcall = stopcall
+  }
+  tick(cb: () => void) {
+    // this order matters
+    cb()
+    this.stopcall()
     return null
   }
 }
@@ -20,36 +30,36 @@ export class Task implements Task {
   name: string
   length: number
   remaining_length: number
-  
+
   constructor(_name: string, _length: number) {
     this.name = _name
     this.length = _length
     // this.remaining_length = _length * 60
-    this.remaining_length = _length * 6000 //dev
-     
-    this.id 
+    this.remaining_length = _length //dev
+
+    this.id
   }
-  
+
   tick(onEnd: () => void) {
-    this.remaining_length ? 
-    this.remaining_length-- :
-    (this.remaining_length = this.length) && (onEnd())
+    console.count("tick")
+    this.remaining_length - 1
+      ? this.remaining_length--
+      : (this.remaining_length = this.length) && onEnd()
   }
 }
 
 export class TaskList implements TaskList {
   name: string
-  tasks: Task[] = [new StopTask()]
+  tasks: Task[] = [new StopTask(() => this.stop())]
   id: string = getUniqueID()
   private looping = false // ignore StopTask or not
   private timer: ReturnType<typeof setTimeout> | null = null
 
   constructor(_name: string, _tasks: Task[]) {
-    
     this.name = _name
-    this.tasks = _tasks
+    this.tasks = [..._tasks.slice(), new StopTask(() => this.stop())]
   }
-  
+
   private get current_task(): Task {
     return this.tasks[0]
   }
@@ -59,23 +69,29 @@ export class TaskList implements TaskList {
   }
 
   send_task_to_bottom() {
+    console.log("send to bottom")
     let wasPlaying = this.isPlaying
     if (this.isPlaying) this.stop()
     let result = this.tasks.slice()
     result.push(result.shift()!)
     this.tasks = result
-    if( wasPlaying ) this.start()
+    if (wasPlaying) this.start()
   }
 
   start() {
+    console.log(this.current_task.name)
     if (this.isPlaying) this.stop()
-    this.timer = setInterval(() => this.current_task.tick(() => this.send_task_to_bottom()), 1000)
+    this.timer = setInterval(
+      () => this.current_task.tick(() => this.send_task_to_bottom()),
+      1000
+    )
   }
 
   stop() {
     if (!this.isPlaying) return
     clearInterval(this.timer!)
     this.timer = null
+    if (this.looping) this.start()
   }
 
   toggle_loop() {
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,6 +1,6 @@
 import { MarkdownView } from "obsidian"
 import { Parser } from "../src/parser"
-import { Task } from "../src/primitives"
+import { Task, TaskList } from "../src/primitives"
 import { describe, expect, test } from "@jest/globals"
 let test_text = `#+ Research
 25 Readings
@@ -45,75 +45,87 @@ Algorithm: 25
 lol
 `
 
-describe("Tasklist Block Parser", () => {
-  test("string should be parsed as a block", () => {
-    expect(Parser.contains_tasklist({ data: test_text } as MarkdownView)).toBe(
-      true
-    )
-  }),
-    test("string should not be parsed as a block", () => {
-      expect(Parser.contains_tasklist({ data: bad_text } as MarkdownView)).toBe(
-        false
-      )
-    })
-})
+let y = new TaskList(
+  'name',
+  [
+    new Task("First", 3),
+    new Task("Second", 3),
+    new Task("Third", 3),
+  ]
+)
 
-describe("Testing Task Parser", () => {
-  test("Should Parse Forward Task Syntax", () => {
-    expect(Parser.Tasks.test("test 30")).toBeInstanceOf(Task)
-  })
-  test("Should Parse Backwards Task Syntax", () => {
-    expect(Parser.Tasks.test("30 test")).toBeInstanceOf(Task)
-  })
-  test("Should Parse Colon Task Syntax", () => {
-    expect(Parser.Tasks.test("test: 30")).toBeInstanceOf(Task)
-  })
-  test("Should Not Parse Colon Task as Forward Task", () => {
-    expect(Parser.Tasks.ForwardTaskValidator.validator.test("test:30")).toBe(
-      false
-    )
-  })
-  test("Should Not Parse Forward Task as Backwards Task", () => {
-    expect(Parser.Tasks.BackwardsTaskValidator.validator.test("test 30")).toBe(
-      false
-    )
-  })
-  test("Should Not Parse Malformed Colon Task as Colon Task", () => {
-    expect(Parser.Tasks.ColonTaskValidator.validator.test("tes:t 30")).toBe(
-      false
-    )
-  })
-})
 
-describe("Testing Command Parser", () => {
-  test("Should Parse Title Syntax", () => {
-    expect(Parser.Commands.test("#+")).toBeInstanceOf(Parser.Commands.CTask)
-  })
-  test("Should Parse Mult Syntax", () => {
-    expect(Parser.Commands.test("x2")).toBeInstanceOf(Parser.Commands.CTask)
-  })
-  test("Should Parse Ref Syntax", () => {
-    expect(Parser.Commands.test("study")).toBeInstanceOf(Parser.Commands.CTask)
-  })
-  test("Should Parse Title with Label Syntax", () => {
-    expect(Parser.Commands.test("#+ My Test Tasklist")).toBeInstanceOf(
-      Parser.Commands.CTask
-    )
-  })
-  test("Should Not Parse Malformed Title Syntax", () => {
-    expect(Parser.Commands.test("#-  ")).toBe(null)
-  })
-  test("Should Not Parse Malformed Mult Syntax", () => {
-    expect(Parser.Commands.test("xx2 ")).toBe(null)
-  })
-  test("Should Not Parse Malformed Ref Syntax", () => {
-    expect(Parser.Commands.test("x1study")).toBe(null)
-  })
-  test("Should Not Parse Malformed Title with Label Syntax", () => {
-    expect(Parser.Commands.test("#- My Test Tasklist ")).toBe(
-      null
-    )
-  })
-})
+y.start() //?
+
+// describe("Tasklist Block Parser", () => {
+//   test("string should be parsed as a block", () => {
+//     expect(Parser.contains_tasklist({ data: test_text } as MarkdownView)).toBe(
+//       true
+//     )
+//   }),
+//     test("string should not be parsed as a block", () => {
+//       expect(Parser.contains_tasklist({ data: bad_text } as MarkdownView)).toBe(
+//         false
+//       )
+//     })
+// })
+
+// describe("Testing Task Parser", () => {
+//   test("Should Parse Forward Task Syntax", () => {
+//     expect(Parser.Tasks.test("test 30")).toBeInstanceOf(Task)
+//   })
+//   test("Should Parse Backwards Task Syntax", () => {
+//     expect(Parser.Tasks.test("30 test")).toBeInstanceOf(Task)
+//   })
+//   test("Should Parse Colon Task Syntax", () => {
+//     expect(Parser.Tasks.test("test: 30")).toBeInstanceOf(Task)
+//   })
+//   test("Should Not Parse Colon Task as Forward Task", () => {
+//     expect(Parser.Tasks.ForwardTaskValidator.validator.test("test:30")).toBe(
+//       false
+//     )
+//   })
+//   test("Should Not Parse Forward Task as Backwards Task", () => {
+//     expect(Parser.Tasks.BackwardsTaskValidator.validator.test("test 30")).toBe(
+//       false
+//     )
+//   })
+//   test("Should Not Parse Malformed Colon Task as Colon Task", () => {
+//     expect(Parser.Tasks.ColonTaskValidator.validator.test("tes:t 30")).toBe(
+//       false
+//     )
+//   })
+// })
+
+// describe("Testing Command Parser", () => {
+//   test("Should Parse Title Syntax", () => {
+//     expect(Parser.Commands.test("#+")).toBeInstanceOf(Parser.Commands.CTask)
+//   })
+//   test("Should Parse Mult Syntax", () => {
+//     expect(Parser.Commands.test("x2")).toBeInstanceOf(Parser.Commands.CTask)
+//   })
+//   test("Should Parse Ref Syntax", () => {
+//     expect(Parser.Commands.test("study")).toBeInstanceOf(Parser.Commands.CTask)
+//   })
+//   test("Should Parse Title with Label Syntax", () => {
+//     expect(Parser.Commands.test("#+ My Test Tasklist")).toBeInstanceOf(
+//       Parser.Commands.CTask
+//     )
+//   })
+//   test("Should Not Parse Malformed Title Syntax", () => {
+//     expect(Parser.Commands.test("#-  ")).toBe(null)
+//   })
+//   test("Should Not Parse Malformed Mult Syntax", () => {
+//     expect(Parser.Commands.test("xx2 ")).toBe(null)
+//   })
+//   test("Should Not Parse Malformed Ref Syntax", () => {
+//     expect(Parser.Commands.test("x1study")).toBe(null)
+//   })
+//   test("Should Not Parse Malformed Title with Label Syntax", () => {
+//     expect(Parser.Commands.test("#- My Test Tasklist ")).toBe(
+//       null
+//     )
+//   })
+// })
 
 export {}
