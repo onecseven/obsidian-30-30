45e2ddf9078ce6852873956108da58d1e8675ffb
https://github.com/onecseven <onecseven@gmail.com>
2023-01-08 16:22:54 -0300

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-import * as React from "react";
+import React from "react"
+import {
+  SendToBottom,
+  DoneBtn,
+  LoopBtn,
+} from "./components/Buttons/SendToBottom"
+import { StartBtn, StopBtn } from "./components/Buttons/PlayBtn"
+import { Icon } from "./components/Icon"
+import { SVGCard } from "./components/TasksLists/SVGCard"
 
-export const ReactView = () => {
-  return <h4>Hello, React!</h4>;
-};
+
+import { useTimerStore } from "./store/store"
+import { TaskList } from "./components/TasksLists/TaskList"
+export const App = () => {
+  let status = useTimerStore((state) => state.status)
+  let looping = useTimerStore((state) => state.looping)
+  return (
+    <>
+      <div className="App">
+        <Icon />
+        {/* <h1> Timer State: {status} </h1>
+          <h2> Looping: {looping ? "Yes" : ""}</h2> */}
+        {/* <StartBtn />No
+          <StopBtn />
+          <SendToBottom />
+          <DoneBtn />
+          <LoopBtn /> */}
+        <TaskList/>
+      </div>
+    </>
+  )
+}
+
+export default App
